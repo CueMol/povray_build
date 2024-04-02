fe9e5c6bff191bcc75f2ded8a85c73795f2028c9
Your Name <you@example.com>
2024-04-02 14:30:12 +0900

    add linux

---
+++ linux/run_build_libpng12.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eux
+
+BASEDIR=$1
+TMPDIR=$BASEDIR/tmp
+
+mkdir -p $TMPDIR
+cd $TMPDIR
+
+# get source
+wget --content-disposition -c https://sourceforge.net/projects/libpng/files/libpng12/1.2.59/libpng-1.2.59.tar.xz
+
+tar xjf libpng-1.2.59.tar.xz
+cd libpng-1.2.59
+
+##########
+
+./configure \
+    --prefix=$BASEDIR/povray_bundle/libpng12 \
+    --disable-shared
+
+##########
+
+make -j 8
+make install
+cd ..
+# rm -rf libpng-1.2.59
